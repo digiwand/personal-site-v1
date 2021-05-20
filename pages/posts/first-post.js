6bbed7d27ee8e61c133c7694c0e96faf1e5bad94
ariellanv <ariellavu@gmail.com>
2021-05-20 18:11:30 -0500

    Next.js tutorial #3 assets & CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,15 @@
 import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-      <>
+      <Layout>
         <h1>First Post</h1>
         <h2>
           <Link href="/">
             <a>Back to home</a>
           </Link>
         </h2>
-      </>
+      </Layout>
     )
   }
