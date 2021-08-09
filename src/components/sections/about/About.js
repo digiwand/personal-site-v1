551788f85b3cad458264ecc61511128568d635e1
ariella <ariellavu@gmail.com>
2021-08-09 16:03:32 -0500

    About: minor updates

--- src/components/sections/about/About.js
+++ src/components/sections/about/About.js
@@ -14,22 +14,22 @@ const AboutSection = React.forwardRef((props, ref) => {
         <Themed.h3>
           About
         </Themed.h3>
-        <Fade bottom delay={420}>
-          <Themed.p sx={{ maxWidth: '560rem' }}>
-            <br />
-            Hello! I'm Ariella Vu.
-            <br />
-            <br />
-            I've been a professional software engineer for over 6 years now. I started my education and career as a full-stack programmer. 
-            Now I specialize in building websites using JavaScript Frameworks (React, Ember, and Angular 1.x). 
-            <br />
-            <br />
-            I'm a native of California. For over the last 3 years, I've worked as a digital nomad. That's right. I have no homebase. In fact, I left my "home" at 14 years old. Fortunately, I've now explored over 30 countries. Overcoming changes and challenges is wired in me.
-            <br />
-            <br />
-            Some of my strengths include problem-solving, adapting, writing clean-code, being detail-oriented, and teamwork. Besides programming and traveling, my passions include yoga, hiking, indoor rock climbing, building new homebases around the world, and personal development.
-          </Themed.p>
-        </Fade>
+      </Fade>
+      <Fade bottom delay={420}>
+        <Themed.p sx={{ maxWidth: '560rem' }}>
+          <br />
+          Hello! I'm Ariella Vu.
+          <br />
+          <br />
+          I've been a professional software engineer for over 6 years now. I started my education and career as a full-stack programmer. 
+          Now I specialize in building websites using JavaScript Frameworks (React, Ember, and Angular 1.x). 
+          <br />
+          <br />
+          I'm a native of California. For over the last 3 years, I've worked as a digital nomad. I've considered myself a nomad since I left my "home" at 14 years old. Fortunately, I've now explored over 30 countries. Overcoming changes and challenges is wired in me.
+          <br />
+          <br />
+          Some of my strengths include problem-solving, adapting, writing clean-code, being detail-oriented, and teamwork. Besides programming and traveling, my passions include yoga, hiking, indoor rock climbing, building new homebases around the world, and personal development.
+        </Themed.p>
       </Fade>
     </Section>
   );
