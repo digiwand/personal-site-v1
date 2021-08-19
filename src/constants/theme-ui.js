63b19560ab2670213c554f22201d3086f6c64f0e
ariella <ariellavu@gmail.com>
2021-08-19 17:28:13 -0500

    theme: allow mint theme

--- src/constants/theme-ui.js
+++ src/constants/theme-ui.js
@@ -2,11 +2,15 @@ import IconNightStay from 'components/svg/material-icons/nightsStay';
 import IconWbSunny from 'components/svg/material-icons/wbSunny';
 
 const THEMEUI_COLOR_MODE_CONFIG = {
+  dark: {
+    icon: <IconNightStay />,
+    displayName: 'dark',
+  },
   light: {
     icon: <IconWbSunny />,
     displayName: 'light',
   },
-  dark: {
+  mint: {
     icon: <IconNightStay />,
     displayName: 'dark',
   },
