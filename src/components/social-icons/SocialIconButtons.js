5067458e2830dac8cfd6ef7139ba0b67b43ab54a
ariella <ariellavu@gmail.com>
2021-08-29 21:40:28 -0500

    SocialIconButtons: rm unnecessary key attr

--- src/components/social-icons/SocialIconButtons.js
+++ src/components/social-icons/SocialIconButtons.js
@@ -20,7 +20,6 @@ function SocialIconButtons({ className, socialKeys }) {
       <Fade cascade key={key} delay={(index + 1) * 255}>
         <IconButton
           className={className}
-          key={key}
         >
           <a href={socialConfig.url} target="_blank" rel="noopener noreferrer">
             {socialConfig.iconSVG}
