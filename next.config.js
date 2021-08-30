4457eb9e34956d4dd461c9741b2dbea1707e4f1d
ariella <ariellavu@gmail.com>
2021-08-29 21:23:07 -0500

    next.config.js: revert all configs

--- next.config.js
+++ next.config.js
@@ -2,20 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  eslint: {
-    dirs: ['components'], // Only run ESLint on the 'components' directories during production builds (next build)
-  },
   webpack: (config, {
     buildId, /* , dev, isServer, defaultLoaders, webpack, */
   }) => {
     console.log('Building: ', buildId);
-
-    config.module.rules.push({
-      test: /\.m?js/,
-      resolve: {
-        fullySpecified: false,
-      },
-    });
 
     // Important: return the modified config
     return config;
