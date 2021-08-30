da7fa971414013ed511c0a9bb42a72e1399387e6
ariella <ariellavu@gmail.com>
2021-08-29 21:39:52 -0500

    index: rm unused comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,23 +22,6 @@ function Home() {
       <WorkSection ref={sectionTrackingPixelRefs[2]} />
       <TechSection ref={sectionTrackingPixelRefs[3]} />
       <ContactSection ref={sectionTrackingPixelRefs[4]} />
-
-      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section> */}
     </Layout>
   );
 }
