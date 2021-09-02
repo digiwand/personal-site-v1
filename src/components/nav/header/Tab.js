cbf54411baac45f1ce909122efb165647d25309e
ariellanv <ariellavu@gmail.com>
2021-09-02 16:49:53 -0500

    NavTabs: add following underline on hover

--- src/components/nav/header/Tab.js
+++ src/components/nav/header/Tab.js
@@ -12,26 +12,29 @@ const defaultProps = {
 };
 
 function NavTab({ children, ...props }) {
-  const { displayName, href, isActive } = props;
+  const {
+    className, displayName, href, isActive,
+  } = props;
 
   return (
     <Link href={href} scroll={false} passHref>
       <a
-        className="NavTab"
+        className={`${className} + NavTab`}
         sx={{
           position: 'relative',
           display: 'inline-flex',
           alignItems: 'center',
           justifyContent: 'center',
-          height: '40px',
-          minWidth: '80rem',
+          height: '40rem',
           letterSpacing: '1.6rem',
           fontSize: '14rem',
-          ml: 3,
-          borderBottom: '3rem solid transparent',
+          borderBottom: '2rem solid transparent',
           transition: 'font-weight 0.2s, color 0.2s, border-bottom 0.2s',
 
-          '&:hover, &:is-active': {
+          '&:hover': {
+            fontWeight: '700',
+          },
+          '&:is-active': {
             fontWeight: '700',
           },
 
