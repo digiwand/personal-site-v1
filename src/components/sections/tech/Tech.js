b53ef49514a389ca2757463aa2793acfb617f8ef
ariellanv <ariellavu@gmail.com>
2021-09-03 12:01:18 -0500

    Tech: add href

--- src/components/sections/tech/Tech.js
+++ src/components/sections/tech/Tech.js
@@ -15,94 +15,127 @@ import {
 
 import { SECTION_ID } from 'constants/section';
 
+const TECH = {
+  NEXTJS: {
+    displayName: 'Next.js',
+    imgElem: <SVGNextJS sx={{ variant: 'images.tech' }} />,
+    href: 'https://nextjs.org/',
+  },
+  REACTJS: {
+    displayName: 'React',
+    imgElem: <Image alt="ReactJS" src="svg/tech/reactjs.svg" variant="tech" />,
+    href: 'https://reactjs.org/',
+  },
+  EMBERJS: {
+    displayName: 'Ember.js',
+    imgElem: <Image alt="EmberJS" src="svg/tech/emberjs.svg" variant="tech" />,
+    href: 'https://emberjs.com/',
+  },
+  HTML5: {
+    displayName: 'HTML5',
+    imgElem: <Image alt="HTML5" src="svg/tech/html5.svg" variant="tech" />,
+    href: 'https://html.spec.whatwg.org/',
+  },
+  SASS: {
+    displayName: 'Sass',
+    imgElem: <Image alt="SASS" src="svg/tech/sass.svg" variant="tech" />,
+    href: 'https://sass-lang.com/',
+  },
+  CHAI: {
+    displayName: 'Chai',
+    imgElem: <Image alt="Chai" src="svg/tech/chai.svg" variant="tech" />,
+    href: 'https://www.chaijs.com/',
+  },
+  MOCHA: {
+    displayName: 'Mocha',
+    imgElem: <Image alt="Mocha" src="svg/tech/mocha.svg" variant="tech" />,
+    href: 'https://mochajs.org/',
+  },
+  AMP_EMAIL: {
+    displayName: 'AMP Email',
+    imgElem: <Image alt="AMP Email" src="svg/tech/amp.svg" variant="tech" />,
+    href: 'https://amp.dev/',
+  },
+  NODEJS: {
+    displayName: 'Node.js',
+    imgElem: <SVGNodeJS sx={{ variant: 'images.tech' }} />,
+    href: 'https://nodejs.org/en/',
+  },
+  GIT: {
+    displayName: 'Git',
+    imgElem: <SVGGit sx={{ variant: 'images.tech' }} />,
+    href: 'https://git-scm.com/',
+  },
+  VS_CODE: {
+    displayName: 'VS Code',
+    imgElem: <Image alt="VS Code" src="svg/tech/vscode.svg" variant="tech" />,
+    href: 'https://code.visualstudio.com/',
+  },
+  ITERM2: {
+    displayName: 'iTerm2',
+    imgElem: <Image alt="iTerm 2" src="svg/tech/iterm2.svg" variant="tech" />,
+    href: 'https://iterm2.com/',
+  },
+  CHROME_DEV_TOOLS: {
+    displayName: 'Chrome Dev Tools',
+    imgElem: <Image alt="Chrome DevTools" src="svg/tech/chromedevtools.svg" variant="tech" />,
+    href: 'https://developer.chrome.com/docs/devtools/',
+  },
+  DASH: {
+    displayName: 'Dash for macOS',
+    imgElem: <Image alt="Dash" src="images/tech/dash@2x.png" variant="tech" />,
+    href: 'https://kapeli.com/dash',
+  },
+  ALFRED: {
+    displayName: 'Alfred for macOS',
+    imgElem: <Image alt="Alfred" src="images/tech/alfred@2x.png" variant="tech" />,
+    href: 'https://www.alfredapp.com/',
+  },
+  MAC_OS: {
+    displayName: 'macOS',
+    imgElem: <Image alt="MacOS" src="svg/tech/macos.svg" variant="tech" />,
+    href: 'https://support.apple.com/en-us/HT201236',
+  },
+};
+
 function TechSection(props, ref) {
   return (
-    <div>
-      <Section
-        id={SECTION_ID.TECH}
-        ref={ref}
-        sx={{
-          background: (t) => `${t.colors.techBg}`,
-          minHeight: '0',
-          py: [5, 6, 6],
-          zIndex: 1,
-        }}
-      >
+    <Section
+      id={SECTION_ID.TECH}
+      ref={ref}
+      sx={{
+        background: (t) => `${t.colors.techBg}`,
+        minHeight: '0',
+        py: [5, 6, 6],
+        zIndex: 1,
+      }}
+    >
 
-        <Fade>
-          <Text
-            variant="sectionHeading"
-            sx={{
-              color: (t) => `${t.colors.techHeader}`,
-              pb: 6,
-            }}
-          >
-            RECENT TECHNOLOGIES
-          </Text>
-        </Fade>
+      <Fade>
+        <Text
+          variant="sectionHeading"
+          sx={{
+            color: (t) => `${t.colors.techHeader}`,
+            pb: 6,
+          }}
+        >
+          RECENT TECHNOLOGIES
+        </Text>
+      </Fade>
 
-        <Fade bottom cascade>
-          <Grid
-            gap={5}
-            columns={[2, null, 5]}
-            sx={{
-              justifyItems: 'center',
-              pb: 4,
-            }}
-          >
-            <SVGNextJS sx={{ variant: 'images.tech' }} />
-            <Image alt="ReactJS" src="svg/tech/reactjs.svg" variant="tech" />
-            <Image alt="EmberJS" src="svg/tech/emberjs.svg" variant="tech" />
-            <Image alt="HTML5" src="svg/tech/html5.svg" variant="tech" />
-            <Image alt="SASS" src="svg/tech/sass.svg" variant="tech" />
-            <Image alt="Chai" src="svg/tech/chai.svg" variant="tech" />
-            <Image alt="Mocha" src="svg/tech/mocha.svg" variant="tech" />
-            <Image alt="AMP Email" src="svg/tech/amp.svg" variant="tech" />
-            <SVGNodeJS sx={{ variant: 'images.tech' }} />
-            <SVGGit sx={{ variant: 'images.tech' }} />
-          </Grid>
-        </Fade>
-      </Section>
-
-      <Section
-        sx={{
-          background: (t) => `${t.colors.techToolsBg}`,
-          minHeight: '0',
-          py: [5, 6, 6],
-          zIndex: 1,
-        }}
-      >
-        <Fade>
-          <Text
-            variant="sectionHeading"
-            sx={{
-              color: (t) => `${t.colors.techToolsHeader}`,
-              pb: 6,
-            }}
-          >
-            RECENT TOOLS
-          </Text>
-        </Fade>
-
-        <Fade bottom cascade>
-          <Grid
-            gap={5}
-            columns={[2, null, 6]}
-            sx={{
-              justifyItems: 'center',
-              pb: 4,
-            }}
-          >
-            <Image alt="VS Code" src="svg/tech/vscode.svg" variant="tech" />
-            <Image alt="iTerm 2" src="svg/tech/iterm2.svg" variant="tech" />
-            <Image alt="Chrome DevTools" src="svg/tech/chromedevtools.svg" variant="tech" />
-            <Image alt="Dash" src="images/tech/dash@2x.png" variant="tech" />
-            <Image alt="Alfred" src="images/tech/alfred@2x.png" variant="tech" />
-            <Image alt="MacOS" src="svg/tech/macos.svg" variant="tech" />
-          </Grid>
-        </Fade>
-      </Section>
-    </div>
+      <Fade bottom cascade>
+        <Grid
+          gap={5}
+          columns={[2, null, 5]}
+          sx={{
+            justifyItems: 'center',
+            pb: 4,
+          }}
+        >
+          {Object.entries(TECH).map(([/* key */, value]) => value.imgElem)}
+        </Grid>
+      </Fade>
+    </Section>
   );
 }
 
