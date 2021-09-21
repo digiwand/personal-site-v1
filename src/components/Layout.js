e38321ceed1287457d3044f8ef7bba3f0056e4bc
ariellanv <ariellavu@gmail.com>
2021-09-21 17:30:17 -0500

    Layout: add charset meta

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -159,6 +159,7 @@ function Layout({ children, sectionTrackingPixelRefs }) {
         <link rel="icon" href="/favicon.ico" />
         <LinkPreloadFonts />
 
+        <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0" />
         <meta
           name="description"
