b4018530c85361e189b1bcdb529d203f72366e99
ariellanv <ariellavu@gmail.com>
2021-09-24 18:00:25 -0500

    constants/tech: alphabetize

--- src/constants/tech.js
+++ src/constants/tech.js
@@ -12,6 +12,7 @@ const TECH = {
   },
   ALFRED: {
     displayName: 'Alfred for macOS',
+    href: 'https://www.alfredapp.com/',
     imgElem: (
       <picture>
         <source srcSet="/images/tech/alfred@2x.webp 2x" type="image/webp" />
@@ -19,7 +20,6 @@ const TECH = {
         <Image alt="Alfred" src="images/tech/alfred@2x.png" variant="tech" />
       </picture>
     ),
-    href: 'https://www.alfredapp.com/',
   },
   ANGULARJS: {
     displayName: 'AngularJS 1.x',
@@ -27,8 +27,8 @@ const TECH = {
   },
   AMP_EMAIL: {
     displayName: 'AMP Email',
-    imgElem: <Image alt="AMP Email" src="svg/tech/amp.svg" variant="tech" />,
     href: 'https://amp.dev/',
+    imgElem: <Image alt="AMP Email" src="svg/tech/amp.svg" variant="tech" />,
   },
   ASP_NET: {
     displayName: 'ASP.NET',
@@ -36,13 +36,13 @@ const TECH = {
   },
   CHAI: {
     displayName: 'Chai',
-    imgElem: <Image alt="Chai" src="svg/tech/chai.svg" variant="tech" />,
     href: 'https://www.chaijs.com/',
+    imgElem: <Image alt="Chai" src="svg/tech/chai.svg" variant="tech" />,
   },
   CHROME_DEV_TOOLS: {
     displayName: 'Chrome Dev Tools',
-    imgElem: <Image alt="Chrome DevTools" src="svg/tech/chromedevtools.svg" variant="tech" />,
     href: 'https://developer.chrome.com/docs/devtools/',
+    imgElem: <Image alt="Chrome DevTools" src="svg/tech/chromedevtools.svg" variant="tech" />,
   },
   CHROME_EXT: {
     displayName: 'Chrome Extensions',
@@ -58,6 +58,7 @@ const TECH = {
   },
   DASH: {
     displayName: 'Dash for macOS',
+    href: 'https://kapeli.com/dash',
     imgElem: (
       <picture>
         <source srcSet="/images/tech/dash@2x.webp 2x" type="image/webp" />
@@ -65,7 +66,6 @@ const TECH = {
         <Image alt="Dash" src="images/tech/dash@2x.png" variant="tech" />
       </picture>
     ),
-    href: 'https://kapeli.com/dash',
   },
   DOCUMENTATION: {
     displayName: 'Documentation',
@@ -73,8 +73,8 @@ const TECH = {
   },
   EMBERJS: {
     displayName: 'Ember.js',
-    imgElem: <Image alt="EmberJS" src="svg/tech/emberjs.svg" variant="tech" />,
     href: 'https://emberjs.com/',
+    imgElem: <Image alt="EmberJS" src="svg/tech/emberjs.svg" variant="tech" />,
   },
   EXPRESSJS: {
     displayName: 'Express.js',
@@ -82,8 +82,8 @@ const TECH = {
   },
   GIT: {
     displayName: 'Git',
-    imgElem: <SVGGit sx={{ variant: 'images.tech' }} />,
     href: 'https://git-scm.com/',
+    imgElem: <SVGGit sx={{ variant: 'images.tech' }} />,
   },
   GOOGLE_WEB_DESIGNER: {
     displayName: 'Google Web Designer',
@@ -119,8 +119,8 @@ const TECH = {
   },
   HTML5: {
     displayName: 'HTML5',
-    imgElem: <Image alt="HTML5" src="svg/tech/html5.svg" variant="tech" />,
     href: 'https://html.spec.whatwg.org/',
+    imgElem: <Image alt="HTML5" src="svg/tech/html5.svg" variant="tech" />,
   },
   INTELLI_J: {
     displayName: 'IntelliJ IDEA',
@@ -128,18 +128,18 @@ const TECH = {
   },
   ITERM2: {
     displayName: 'iTerm2',
-    imgElem: <Image alt="iTerm 2" src="svg/tech/iterm2.svg" variant="tech" />,
     href: 'https://iterm2.com/',
+    imgElem: <Image alt="iTerm 2" src="svg/tech/iterm2.svg" variant="tech" />,
   },
   MAC_OS: {
     displayName: 'macOS',
-    imgElem: <Image alt="MacOS" src="svg/tech/macos.svg" variant="tech" />,
     href: 'https://support.apple.com/en-us/HT201236',
+    imgElem: <Image alt="MacOS" src="svg/tech/macos.svg" variant="tech" />,
   },
   MOCHA: {
     displayName: 'Mocha',
-    imgElem: <Image alt="Mocha" src="svg/tech/mocha.svg" variant="tech" />,
     href: 'https://mochajs.org/',
+    imgElem: <Image alt="Mocha" src="svg/tech/mocha.svg" variant="tech" />,
   },
   MONGODB: {
     displayName: 'MongoDB',
@@ -147,13 +147,13 @@ const TECH = {
   },
   NEXTJS: {
     displayName: 'Next.js',
-    imgElem: <SVGNextJS sx={{ variant: 'images.tech' }} />,
     href: 'https://nextjs.org/',
+    imgElem: <SVGNextJS sx={{ variant: 'images.tech' }} />,
   },
   NODEJS: {
     displayName: 'Node.js',
-    imgElem: <SVGNodeJS sx={{ variant: 'images.tech' }} />,
     href: 'https://nodejs.org/en/',
+    imgElem: <SVGNodeJS sx={{ variant: 'images.tech' }} />,
   },
   POSTGRESQL: {
     displayName: 'PostgreSQL',
@@ -161,8 +161,8 @@ const TECH = {
   },
   REACTJS: {
     displayName: 'React',
-    imgElem: <Image alt="ReactJS" src="svg/tech/reactjs.svg" variant="tech" />,
     href: 'https://reactjs.org/',
+    imgElem: <Image alt="ReactJS" src="svg/tech/reactjs.svg" variant="tech" />,
   },
   REGEX: {
     displayName: 'Regex',
@@ -170,13 +170,13 @@ const TECH = {
   },
   SASS: {
     displayName: 'Sass',
-    imgElem: <Image alt="SASS" src="svg/tech/sass.svg" variant="tech" />,
     href: 'https://sass-lang.com/',
+    imgElem: <Image alt="SASS" src="svg/tech/sass.svg" variant="tech" />,
   },
   VS_CODE: {
     displayName: 'VS Code',
-    imgElem: <Image alt="VS Code" src="svg/tech/vscode.svg" variant="tech" />,
     href: 'https://code.visualstudio.com/',
+    imgElem: <Image alt="VS Code" src="svg/tech/vscode.svg" variant="tech" />,
   },
 };
 
