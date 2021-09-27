e10ec03e00fa4bc9d65559c90a9083e14617fc8b
ariellanv <ariellavu@gmail.com>
2021-09-27 17:33:57 -0500

    next.config: add locale us

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+
   poweredByHeader: false,
+
   webpack: (config, {
     buildId, /* , dev, isServer, defaultLoaders, webpack, */
   }) => {
