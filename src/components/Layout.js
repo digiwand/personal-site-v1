f18894f421ea55a0ef761e35631146b4d9c0f8e7
ariellanv <ariellavu@gmail.com>
2021-09-26 22:36:52 -0500

    Layout: add link crossOrigin="anonymous" https://stackoverflow.com/questions/1330825/preloading-font-face-fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -25,70 +25,70 @@ function LinkPreloadFonts() {
         rel="preload"
         href="/fonts/BarlowCondensed-Regular.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
       <link
         rel="preload"
         href="/fonts/BarlowCondensed-Thin.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
 
       <link
         rel="preload"
         href="/fonts/MarckScript-Regular.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
 
       <link
         rel="preload"
         href="/fonts/Roboto/Roboto-Light.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
       <link
         rel="preload"
         href="/fonts/Roboto/Roboto-Bold.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
       <link
         rel="preload"
         href="/fonts/Roboto/Roboto-Medium.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
       <link
         rel="preload"
         href="/fonts/Roboto/Roboto-Regular.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
 
       <link
         rel="preload"
         href="/fonts/Rubik/static/Rubik-Bold.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
       <link
         rel="preload"
         href="/fonts/Rubik/static/Rubik-Light.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
       <link
         rel="preload"
         href="/fonts/Rubik/static/Rubik-Medium.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
       <link
         rel="preload"
         href="/fonts/Rubik/static/Rubik-Regular.ttf"
         as="font"
-        crossOrigin=""
+        crossOrigin="anonymous"
       />
     </>
   );
