84a4601808e9d4aa562b520668fbd549dd79a461
ariellanv <ariellavu@gmail.com>
2021-11-10 19:15:10 +0000

    tsx: constants/theme-ui

---
+++ src/constants/theme-ui.tsx
@@ -0,0 +1,15 @@
+import IconNightStay from 'components/svg/material-icons/nightsStay';
+import IconWbSunny from 'components/svg/material-icons/wbSunny';
+
+const THEMEUI_COLOR_MODE_CONFIG = {
+  dark: {
+    icon: <IconNightStay />,
+    displayName: 'dark',
+  },
+  light: {
+    icon: <IconWbSunny />,
+    displayName: 'light',
+  },
+};
+
+export default THEMEUI_COLOR_MODE_CONFIG;
