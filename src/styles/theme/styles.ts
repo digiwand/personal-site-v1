9670e42fe9007249118bbcc7aacf3a589d4b7b30
digiwand <20778143+digiwand@users.noreply.github.com>
2023-08-11 12:38:37 +0200

    style: mv text styles to root

--- src/styles/theme/styles.ts
+++ src/styles/theme/styles.ts
@@ -10,41 +10,42 @@ const styles: Theme['styles'] = {
       variant: 'text.body',
       fontSize: 1,
     },
-  },
 
-  h1: {
-    variant: 'text.heading',
-    fontSize: [6, 7, 7],
-  },
-  h2: {
-    variant: 'text.heading',
-    fontSize: [4, 5, 5],
-    letterSpacing: ['1rem', '3rem', '3rem'],
-  },
-  h3: {
-    variant: 'text.heading',
-    fontWeight: '400',
-    fontSize: [3, 4, 4],
-    letterSpacing: '5rem',
-  },
-  h4: {
-    variant: 'text.heading',
-    fontWeight: '500',
-    fontSize: [2, 3, 3],
-    letterSpacing: ['1rem', '1.6rem', '1.6rem'],
-  },
-  h5: {
-    variant: 'text.heading',
-    fontWeight: [300, 400, 400],
-    letterSpacing: ['1rem', '1.2rem', '1.2rem'],
-    fontSize: 1,
-  },
-  h6: {
-    variant: 'text.heading',
-    fontSize: 0,
-  },
-  p: {
-    fontSize: 1,
+    h1: {
+      variant: 'text.heading',
+      fontSize: [6, 7, 7],
+    },
+    h2: {
+      variant: 'text.heading',
+      fontSize: [4, 5, 5],
+      letterSpacing: ['1rem', '3rem', '3rem'],
+    },
+    h3: {
+      variant: 'text.heading',
+      fontWeight: '400',
+      fontSize: [3, 4, 4],
+      letterSpacing: '5rem',
+    },
+    h4: {
+      variant: 'text.heading',
+      fontWeight: '500',
+      fontSize: [2, 3, 3],
+      letterSpacing: ['1rem', '1.6rem', '1.6rem'],
+    },
+    h5: {
+      variant: 'text.heading',
+      fontWeight: [300, 400, 400],
+      letterSpacing: ['1rem', '1.2rem', '1.2rem'],
+      fontSize: 1,
+    },
+    h6: {
+      variant: 'text.heading',
+      fontSize: 0,
+    },
+    
+    p: {
+      fontSize: 1,
+    },
   },
 };
 
