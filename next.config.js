708983012f2dcaf1db745230e52ae7450ba233e7
digiwand <20778143+digiwand@users.noreply.github.com>
2026-05-06 17:22:48 -0700

    build: rm redundant i18n nextConfig

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const path = require('path');
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US',
-  },
-
   poweredByHeader: false,
 
   outputFileTracingRoot: path.join(__dirname, '..'),
