5e4f32f522710c7ff12cd1f56f12812851c80421
digiwand <20778143+digiwand@users.noreply.github.com>
2026-05-11 00:55:29 -0700

    build: rm next.config.js outputFileTracingRoot

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const path = require('path');
-
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   poweredByHeader: false,
-
-  outputFileTracingRoot: path.join(__dirname, '..'),
 };
 
 module.exports = nextConfig;
