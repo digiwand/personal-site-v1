052a061d8550364311a066b628e3c3f9a9c7b250
digiwand <20778143+digiwand@users.noreply.github.com>
2026-09-01 15:46:23 -0700

    feat: Add Resume Download Icon (#84)
    * feat: add download icon animation
    
    * feat: update Resume download animation
    - simpler
    
    * Revert "feat: update Resume download animation"
    
    This reverts commit 6a4e77a675bc4bcc8fb7320abac959174449d7be.
    
    * feat: add resume btn icon 1rem pt

--- src/components/resume-button/ResumeButton.tsx
+++ src/components/resume-button/ResumeButton.tsx
@@ -1,15 +1,57 @@
-import { Button } from 'theme-ui';
+import { getColor } from "@theme-ui/color";
+import { Button } from "theme-ui";
+import IconDownload from "components/svg/material-icons/download";
 
 type Props = {
   className?: string;
-}
+};
 
-function ResumeButton({ className = '' }: Props) {
+function ResumeButton({ className = "" }: Props) {
   return (
-    <Button className={className}>
+    <Button
+      className={className}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        margin: '0 auto',
+
+        '.resume-icon-container': {
+          display: 'flex',
+          justifyContent: 'end',
+          transition: 'width 0.1s',
+          width: '0',
+          overflow: 'hidden',
+          pt: '1rem',
+
+          '> span': {
+            display: 'flex',
+            alignItems: 'center',
+          },
+
+          svg: {
+            position: 'relative',
+            height: '24rem',
+            width: '24rem',
+          },
+          path: {
+            fill: (t) => getColor(t, 'buttonText'),
+            opacity: '0.7',
+          },
+        },
+
+        '&:hover .resume-icon-container': {
+          width: '32rem',
+        },
+      }}
+    >
       <a href="/Resume-Ariella-Vu.pdf" download>
         RÉSUMÉ
       </a>
+      <div className="resume-icon-container">
+        <span>
+          <IconDownload />
+        </span>
+      </div>
     </Button>
   );
 }
